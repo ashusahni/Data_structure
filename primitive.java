0e0b50a7cf73a139384c1db3ca1ee6693c4359d6
ashutosh <ashutoshsahni833@gmail.com>
2025-02-24 22:33:11 +0530

    conditional loops

---
+++ primitive.java
@@ -0,0 +1,12 @@
+
+
+public class primitive {
+    public static void main(String[] args) {
+    int rollno =34;
+    char letter = 'a';
+    float marks = 8834.445f;
+    double largeDecimalNumbers = 43783434.434232;
+    long largeInteger = 42344324343432443L;
+
+    }
+}
