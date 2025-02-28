df7a4fe00f27513e2d783c1d4d70958a6a2c84a3
ashutosh <ashutoshsahni833@gmail.com>
2025-03-01 00:02:29 +0530

    Params

---
+++ Params.java
@@ -0,0 +1,14 @@
+public class Params {
+    public static void main(String[] args) {
+        int ans = sum(39,34);
+
+        System.out.println(ans);
+    }
+
+    static int sum(int a, int b){
+        int adding = a + b;
+        return adding;
+    }
+
+
+}
