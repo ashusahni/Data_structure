6485def994f544cd956ddee824964cf132b1b248
ashutosh <ashutoshsahni833@gmail.com>
2025-05-27 09:02:02 +0530

    srchinrange

---
+++ src/ashu/SearchInRange.java
@@ -0,0 +1,26 @@
+package ashu;
+
+import com.sun.source.tree.BreakTree;
+
+public class SearchInRange  {
+    public static void main(String[] args) {
+        int[] arr = {34,3,235,32,65,65,312,54};
+        int target = 65;
+        System.out.println(linearSearch(arr,target,2,6));
+    }
+
+    static  int linearSearch(int[] arr, int target,int start, int end){
+        if(arr.length==0){
+            return  -1;
+        }
+        for(int index=start; index<=end; index++){
+            int element = arr[index];
+                    if(element==target){
+                        return  index;
+                    }
+        }
+        return -1;
+    }
+
+
+}
