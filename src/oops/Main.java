dc52dfa4d42d8199cb4c64894238f61e1841ec13
ashutosh <ashutoshsahni833@gmail.com>
2025-06-04 00:31:58 +0530

    m

--- src/oops/Main.java
+++ src/oops/Main.java
@@ -1,4 +1,8 @@
-package OOPS;
+package oops;
 
 public class Main {
+    public static void main(String[] args) {
+    Human Ashutosh = new Human(22,"Ashutosh",100000,false);
+        System.out.println(Ashutosh.name);
+    }
 }
