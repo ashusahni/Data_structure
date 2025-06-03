6d6eae3c860f5abd27ff4d797ec8207bcbfb1888
ashutosh <ashutoshsahni833@gmail.com>
2025-06-03 08:52:30 +0530

    la

--- src/intro/Largest.java
+++ src/intro/Largest.java
@@ -1,4 +1,25 @@
 package intro;
 
+import java.util.Scanner;
+
 public class Largest {
+    public static void main(String[] args) {
+
+    Scanner in = new Scanner(System.in);
+    int a = in.nextInt();
+    int b = in.nextInt();
+    int c = in.nextInt();
+
+    int largest = a;
+    if(b > largest){
+        largest = b;
+    }
+    if (c > largest){
+        largest=c;
+    }
+        System.out.println(largest);
+
+
+    }
 }
+
