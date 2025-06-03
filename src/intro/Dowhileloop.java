a577fbfe9eb4130550412e279a421c39b5d37b52
ashutosh <ashutoshsahni833@gmail.com>
2025-06-03 08:52:12 +0530

    doLIpe

--- src/intro/Dowhileloop.java
+++ src/intro/Dowhileloop.java
@@ -3,5 +3,12 @@ package intro;
 public class Dowhileloop {
     public static void main(String[] args) {
 
+            int num = 1 ;
+
+            do {
+                System.out.println("down to atlanat");
+
+            }while (num!=1);
+
     }
 }
