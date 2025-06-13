a82436bf44f5582ccefb5e0083103ab10b5838d5
ashutosh <ashutoshsahni833@gmail.com>
2025-06-13 23:53:52 +0530

    peakELemErrorfx

--- src/binary_search/interviewPrep/FindPeakElement.java
+++ src/binary_search/interviewPrep/FindPeakElement.java
@@ -1,4 +1,26 @@
 package binary_search.interviewPrep;
 
 public class FindPeakElement {
+    while (start <= end) {
+        int mid = start + (end - start) / 2;
+
+        if (arr[mid] == target)
+            return mid;
+
+        if (isAsc) {
+            if (target < arr[mid]) {
+                end = mid - 1;
+            } else {
+                start = mid + 1;
+            }
+        } else {
+            if (target > arr[mid]) {
+                end = mid - 1;
+            } else {
+                start = mid + 1;
+            }
+        }
+    }
+        return -1;
 }
+
