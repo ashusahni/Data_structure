fddd16e1ebab65fb4296f740ac39f69ccb348eaa
ashutosh <ashutoshsahni833@gmail.com>
2025-06-27 23:35:45 +0530

    updated search rotated

--- src/binary_search/interviewPrep/SearchInRotated.java
+++ src/binary_search/interviewPrep/SearchInRotated.java
@@ -9,29 +9,29 @@ public class SearchInRotated {
 
     }
     public static int search(int[] nums, int target) {
-        int left = 0;
-        int right = nums.length - 1;
+        int start = 0;
+        int end = nums.length - 1;
 
-        while (left <= right) {
-            int mid = left + (right - left) / 2;
+        while (start <= end) {
+            int mid = start + (end - start) / 2;
 
             // Found the target
             if (nums[mid] == target)
                 return mid;
 
             // Left half is sorted
-            if (nums[left] <= nums[mid]) {
-                if (nums[left] <= target && target < nums[mid])
-                    right = mid - 1;
+            if (nums[start] <= nums[mid]) {
+                if (nums[start] <= target && target < nums[mid])
+                    end = mid - 1;
                 else
-                    left = mid + 1;
+                    start = mid + 1;
 
                 // Right half is sorted
             } else {
-                if (nums[mid] < target && target <= nums[right])
-                    left = mid + 1;
+                if (nums[mid] < target && target <= nums[end])
+                    start = mid + 1;
                 else
-                    right = mid - 1;
+                    end = mid - 1;
             }
         }
 
