a1b2e208f97f1c60105e23ea9b91a118a010cf59
Ashutosh <142080355+ashusahni@users.noreply.github.com>
2025-08-20 23:43:28 +0530

    Update Main.java

--- src/oops/Main.java
+++ src/oops/Main.java
@@ -3,7 +3,7 @@ package oops;
 public class Main {
     public static void main(String[] args) {
     Human Ashutosh = new Human(22,"Ashutosh",100000,false);
-        System.out.println(Ashutosh.name);
+       
         message();
     }
       static void message(){
